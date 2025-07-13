e7e6368305eb9e2b86e6e322cf812e99ce4208c9
eugediazsegura <eugeniaa.ds@gmail.com>
2025-07-13 02:13:30 +0200

    style: Define button View More Products

--- src/components/ui/ButtonSecondary.tsx
+++ src/components/ui/ButtonSecondary.tsx
@@ -5,6 +5,6 @@ type ButtonSecondaryProps = {
 
 export const ButtonSecondary = ({ onClick, data }: ButtonSecondaryProps) => {
     return (
-        <button type="button" onClick={onClick}>{data}</button>
+        <button type="button" className="border-2 border-pink-light hover:border-pink-dark text-pink-light hover:text-pink-dark rounded-full py-2 px-1 w-1/5" onClick={onClick}>{data}</button>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
 import { Sidebar } from "@/components/layout/Sidebar";
 import { ProductList } from "@/components/product/ProductList";
 import { BannerParallax } from "@/components/ui/BannerParallax";
+import { ButtonSecondary } from "@/components/ui/ButtonSecondary";
 
 
 const Home = () => {
     return (
-        <div>
+        <div className="home">
             <BannerParallax />
-            <div className=" flex justify-between my-14">
-                <Sidebar />
-                <ProductList />
+            <div className="products-container  flex flex-col items-center">
+                <div className=" flex justify-between my-14">
+                    <Sidebar />
+                    <ProductList />
+                </div>
+                <ButtonSecondary onClick={() => { }} data="View more products" />
             </div>
 
         </div>
